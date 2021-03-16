6c84bb6049af4b882ee08f4f986bb0ad264006b9
Axel Duret <axelfaya@gmail.com>
2021-03-16 12:16:09 +0100

    update migration files

--- mykonos/database/migrations/2021_03_16_093306_create_taxonomies_table.php
+++ mykonos/database/migrations/2021_03_16_093306_create_taxonomies_table.php
@@ -6,26 +6,27 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateTaxonomiesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('taxonomies', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('taxonomies', function (Blueprint $table) {
+      $table->id();
+      $table->timestamps();
+      $table->string('label', 100);
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('taxonomies');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('taxonomies');
+  }
 }
