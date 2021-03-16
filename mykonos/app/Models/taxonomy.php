28d48f401d6f800b876ff4fbaad23ce71c6e2135
Axel Duret <axelfaya@gmail.com>
2021-03-16 14:31:00 +0100

    add methods files

--- mykonos/app/Models/taxonomy.php
+++ mykonos/app/Models/taxonomy.php
@@ -5,7 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class taxonomy extends Model
+class Taxonomy extends Model
 {
-    use HasFactory;
+  use HasFactory;
+
+  protected $fillable = [
+    'label'
+  ];
+
+  protected $hidden = [
+    'created_at',
+    'updated_at',
+  ];
+
+  public function posts()
+  {
+    return $this->belongsToMany(Post::class);
+  }
 }
