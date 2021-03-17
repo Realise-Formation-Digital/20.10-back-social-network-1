f762bc6a048f8bdf412f13d496799d99689688f4
Fabian Corrales <fabian_corrales@windowslive.com>
2021-03-17 11:26:44 +0100

    test toolbar
    creation d'une toolbar

--- mykonos/resources/js/components/Home.vue
+++ mykonos/resources/js/components/Home.vue
@@ -1,7 +1,21 @@
-<template><v-card
-  elevation="3"
-  shaped
-><v-btn>test button</v-btn></v-card>
+<template>
+  <div>
+    <v-toolbar
+      dark
+      prominent
+      src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </div>
   
 </template>
 
