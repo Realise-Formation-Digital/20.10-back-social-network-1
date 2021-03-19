fe5da5f70feec6d803d1049269d4737f9a0daaa7
Boo <aurelieboo1219@gmail.com>
2021-03-19 11:25:53 +0100

    Taxonomy ressources

--- mykonos/app/Http/Resources/TaxonomyResource.php
+++ mykonos/app/Http/Resources/TaxonomyResource.php
@@ -14,6 +14,11 @@ class TaxonomyResource extends JsonResource
    */
   public function toArray($request)
   {
+    return [
+      'id' => $this->id,
+      'label'=> $this->label,
+      'posts'=> PostResource::collection($this->posts),
+    ];
     return parent::toArray($request);
   }
 }
