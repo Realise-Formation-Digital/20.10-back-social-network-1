df23459bbb3af24aa4506534a36dc6cbbf815e4b
Axel Duret <axelfaya@gmail.com>
2021-03-23 16:16:46 +0100

    edit home.vue

--- mykonos/resources/js/components/pages/Home.vue
+++ mykonos/resources/js/components/pages/Home.vue
@@ -1,16 +1,24 @@
 <template>
-  <v-flex xs12 sm12 id="sign-in-text-wrapper">
-    <v-layout column align-center justify-center fill-height pa-3>
-      <div class="login-wrapper text-xs-center mb-3">
-        <div class="display-1 font-weight-black mb-3">
-          Bon retour parmi nous!
-        </div>
-      </div>
-      <v-btn class="mt-4 white--text indigo" rounded large to="/login"
-        >Connexion</v-btn
-      >
+  <v-container fluid pa-0>
+    <v-layout row wrap>
+      <v-flex xs12 sm12 id="sign-up-form-wrapper" class="active">
+        <v-layout column align-center justify-center pa-3 mt-5>
+          <v-flex xs12 mb-3 mt-5>
+            <div class="login-wrapper text-xs-center">
+              <div class="display-1 font-weight-black mb-3">
+                Bon retour parmi nous!
+              </div>
+              <v-layout column align-center justify-center pa-3 mt-5>
+                <v-btn class="mt-4 white--text indigo" rounded large to="/login"
+                  >Connexion</v-btn
+                >
+              </v-layout>
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-flex>
     </v-layout>
-  </v-flex>
+  </v-container>
 </template>
 
 <script>
