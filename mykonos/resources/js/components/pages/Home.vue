ea5b2a3a463317bda4cf0a615a871287bf37c2af
Axel Duret <axelfaya@gmail.com>
2021-03-25 08:34:02 +0100

    responsive homepage

--- mykonos/resources/js/components/pages/Home.vue
+++ mykonos/resources/js/components/pages/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <v-row
-      ><v-col cols="3"> <app-sidebar></app-sidebar></v-col
-      ><v-col cols="9"> <app-posts></app-posts></v-col
+      ><v-col cols="0" lg="3"> <app-sidebar></app-sidebar></v-col
+      ><v-col cols="12" lg="9"> <app-posts></app-posts></v-col
     ></v-row>
   </div>
 </template>
--- mykonos/resources/js/components/pages/Sidebar.vue
+++ mykonos/resources/js/components/pages/Sidebar.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-card class="mx-auto ml-1" height="100%" width="300">
-    <v-navigation-drawer absolute width="300" permanent>
+  <v-card class="mx-auto ml-1" height="100%" width="100%">
+    <v-navigation-drawer absolute width="100%" permanent>
       <app-taxonomy></app-taxonomy>
     </v-navigation-drawer>
   </v-card>
